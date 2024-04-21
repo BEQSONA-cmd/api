b8805f708cb636c2a5c5f6ca684f2240cd211b34
Beqa Tvildiani <btvildia@c2r8s8.42wolfsburg.de>
2024-04-21 15:29:52 +0200

    api

---
+++ install.sh
@@ -0,0 +1,16 @@
+ssh ubuntu@132.145.250.27 -i ~/.ssh/Chxikvia.key
+https://check-host.net/
+sudo ufw allow http
+sudo ufw reload
+sudo ufw allow 8080/tcp
+sudo ufw reload
+sudo ufw status
+```iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT```
